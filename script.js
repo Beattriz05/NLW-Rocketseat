64a19dc260accae38c154afa038de9db3867eb89
Beatriiz05 <anabeatrizdasilva263@gmail.com>
2025-07-13 15:24:43 -0300

    aula #3

--- script.js
+++ script.js
@@ -5,16 +5,52 @@ const askButton = document.querySelector('button');
 const aiResponse = document.getElementById('aiResponse');
 const form = document.getElementById('form');
 
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter();
+    return converter.makeHtml(text);
+}
+
 const perguntarAI = async (question, game,apiKey) => {
     const model = "gemini-2.5-flash";
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
     const pergunta = `
-    Você é um especialista em ${game}. Responda a pergunta de forma clara e objetiva, com base no conhecimento mais recente sobre o jogo. Pergunta: ${question}
+    ## Especialidade:
+    Você é um especialista assistente de meta para o jogo ${game} e pode responder perguntas sobre o jogo.
+
+    ## Tarefa:
+    Responda a pergunta do usuário com base no seu conhecimento do jogo, estratégias,build e dicas, assim forneça informações úteis e relevantes.
+
+    ## Regras:
+    - Responda apenas com informações sobre o jogo.
+    - Se você não souber a resposta, diga que 'Não sei' e não tente inventar uma resposta.
+    - Se a pergunta não for sobre o jogo, responda com 'Não posso ajudar com isso'.
+    - Se a pergunta não for clara, peça mais detalhes.
+    - Use uma linguagem clara e objetiva.
+    - Considere a data atual ${new Date().toLocaleDateString} e as atualizações mais recentes do jogo.
+    - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para fornecer informações precisas e relevantes.
+    - Nunca responda itens que você não tem certeza de que existe no patch atual.
+
+    ## Resposta
+    - Economize na resposta, seja direto e responda no máximo 500 caracteres
+    - Responda em markdown
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+    ## Exemplo de resposta
+    pergunta do usuário: Melhor build rengar jungle
+    resposta: A build mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\nexemplo de runas\n\n
+
+    ---
+    Pergunta do usuário: ${question}
     `
     const contents = [{
+        role: 'user',
         parts: [{
             text: pergunta
         }]
+    }];
+
+    const tools = [{
+        google_search: {}
     }];
 
     // Chamada para a API Gemini
@@ -24,7 +60,8 @@ const perguntarAI = async (question, game,apiKey) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            contents
+            contents,
+            tools
         })
     })
 
@@ -50,7 +87,8 @@ const enviarFormulario = async (event) => {
     // Envia a pergunta para IA
     try {
         const text = await perguntarAI(question, game, apiKey)
-        aiResponse.querySelector('.response-content').innerHTML = text;
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text);
+        aiResponse.classList.remove('hidden');
     } catch (error) {
         console.log('Erro ao enviar a pergunta:', error);
     } finally {
